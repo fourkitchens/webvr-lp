c3307a694fd90c53b23dfeb4a0216c1712fba8f7
Randy Oest <oest@amazingrando.com>
2018-03-07 13:25:57 -0500

    Prettier test

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,8 @@
-import React from "react";
-import Link from "gatsby-link";
-import styles from "./button.module.scss";
-import cardboard from "../../assets/cardboard.svg";
-import PropTypes from "prop-types";
+import React from 'react';
+import Link from 'gatsby-link';
+import styles from './button.module.scss';
+import cardboard from '../../assets/cardboard.svg';
+import PropTypes from 'prop-types';
 
 const Button = props => (
   <Link to={props.link} className={styles.button}>
@@ -15,7 +15,7 @@ const Button = props => (
 
 Button.propTypes = {
   link: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
 };
 
 export default Button;
