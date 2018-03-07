3a905c7d32e14ff2616ede0ce263837f985caf6e
Randy Oest <oest@amazingrando.com>
2018-03-07 11:48:37 -0500

    Add gh-pages for deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: "Gatsby Default Starter"
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      },
-    },{
+        pathToConfigModule: `src/utils/typography.js`
+      }
+    },
+    {
       resolve: `gatsby-plugin-postcss-sass`,
       options: {
         // postCssPlugins: [somePostCssPlugin()],
-        precision: 8, // SASS default: 5
-      },
-    },],
+        precision: 8 // SASS default: 5
+      }
+    }
+  ],
+  pathPrefix: `/webvr-lp`
 };
