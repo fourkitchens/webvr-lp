c1346e5b9c404691288ef09d22c7acf2b3d71d77
Randy Oest <oest@amazingrando.com>
2018-03-08 17:06:46 -0500

    Adding social sharing components

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import promoImage from '../assets/promo-image.jpg';
+
+const metaInfo = {
+  title: 'Web VR for Everyone | Four Kitchens',
+  description:
+    'Four Kitchens creates WebVR for Everyone. Explore the experiences we’ve made using the phone & computer you already have.',
+  keywords:
+    'immersive technology , android, ar, article, augmented, augmented reality, browser, device, experience, google, headset, phone, reality, space, technologies, virtual reality, vr, web, webar, webvr',
+  url: 'http://vr.fourkitchens.com',
+};
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Web VR for Everyone | Four Kitchens"
+      title={metaInfo.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: metaInfo.description },
+        { name: 'keywords', content: metaInfo.keywords },
+        { name: 'og:title', content: metaInfo.title },
+        { name: 'og:description', content: metaInfo.description },
+        { name: 'og:image', content: promoImage },
+        { name: 'og:url', content: metaInfo.url },
+        { name: 'twitter:card', content: 'summary_large_image' },
       ]}
       link={[
         {
