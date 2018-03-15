4645c8c22536d45b775a87ae6d774e6122fb48c6
Michal Minecki <michal@mirzu.com>
2018-03-15 17:48:12 -0500

    added ga tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,14 @@ module.exports = {
         // postCssPlugins: [somePostCssPlugin()],
         precision: 8 // SASS default: 5
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-559851-16",
+        // Puts tracking script in the head instead of the body
+        head: false,
+      },
     }
   ],
   pathPrefix: `/webvr-lp`
